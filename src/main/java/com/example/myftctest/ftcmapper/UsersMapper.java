b75764c1784714ea8a3667460ebce81a6c2388ef
gq544 <woshigq513@163.com>
2019-07-17 15:49:16 +0800

    1

--- src/main/java/com/example/myftctest/ftcmapper/UsersMapper.java
+++ src/main/java/com/example/myftctest/ftcmapper/UsersMapper.java
@@ -3,13 +3,13 @@ package com.example.myftctest.ftcmapper;
 import com.example.myftctest.ftcentity.Users;
 
 public interface UsersMapper {
-    int deleteByPrimaryKey(Integer likeid);
+    int deleteByPrimaryKey(Integer userid);
 
     int insert(Users record);
 
     int insertSelective(Users record);
 
-    Users selectByPrimaryKey(Integer likeid);
+    Users selectByPrimaryKey(Integer userid);
 
     int updateByPrimaryKeySelective(Users record);
 
